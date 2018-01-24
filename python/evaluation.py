d1d69de277dc9f1c661a0394b7bf1065db73e072
Kevin Egger <eggerk@ethz.ch>
2018-01-24 14:53:38 +0100

    Autoformat

--- python/evaluation.py
+++ python/evaluation.py
@@ -8,53 +8,57 @@ import utils as eval_utils
 from job import Job
 import IPython
 
+
 class Evaluation(object):
 
-    def __init__(self, job_dir, root_folder):
-        logging.basicConfig(level=logging.DEBUG)
-        self.logger = logging.getLogger(__name__)
-        self.job_dir = job_dir
-        self.root_folder = root_folder
-        
-        job_filename = os.path.join(job_dir, 'job.yaml')
-        if not os.path.isfile(job_filename):
-            raise ValueError("Job info file does not exist: " + job_filename)
-        self.job = yaml.safe_load(open(job_filename))
-        self.evaluation_scripts = []
-        if "evaluation_scripts" in self.job and self.job['evaluation_scripts'] is not None:
-            self.evaluation_scripts = self.job["evaluation_scripts"]
-            self.logger.info("Registering " + str(len(self.evaluation_scripts)) \
-                             + " evaluation scripts.")
-        else:
-            self.logger.info("No evaluation scripts in job.")
-        
-    def runEvaluations(self):
-        for evaluation in self.evaluation_scripts:
-            self.logger.info("=== Run Evaluation ===")
-            if 'name' not in evaluation:
-              raise Exception("Missing name tag")
-            evaluation_script = evaluation['name']
-            evaluation_script_with_path = self.root_folder + '/evaluation/' + evaluation_script
+  def __init__(self, job_dir, root_folder):
+    logging.basicConfig(level=logging.DEBUG)
+    self.logger = logging.getLogger(__name__)
+    self.job_dir = job_dir
+    self.root_folder = root_folder
 
-            jp = Job()
-            jp.setPythonExecutable(evaluation_script_with_path)
-            jp.addParam("data_dir", self.job_dir)
-            jp.addParam("localization_map", self.job["localization_map"])
-            if "parameter_file" in self.job:
-                jp.addParam("parameter_file", self.job["parameter_file"])
-            if "dataset" in self.job:
-                jp.addParam("dataset", self.job["dataset"])
-            jp.execute()
+    job_filename = os.path.join(job_dir, 'job.yaml')
+    if not os.path.isfile(job_filename):
+      raise ValueError("Job info file does not exist: " + job_filename)
+    self.job = yaml.safe_load(open(job_filename))
+    self.evaluation_scripts = []
+    if "evaluation_scripts" in self.job and \
+        self.job['evaluation_scripts'] is not None:
+      self.evaluation_scripts = self.job["evaluation_scripts"]
+      self.logger.info("Registering " + str(len(self.evaluation_scripts)) \
+                       + " evaluation scripts.")
+    else:
+      self.logger.info("No evaluation scripts in job.")
+
+  def runEvaluations(self):
+    for evaluation in self.evaluation_scripts:
+      self.logger.info("=== Run Evaluation ===")
+      if 'name' not in evaluation:
+        raise Exception("Missing name tag")
+      evaluation_script = evaluation['name']
+      evaluation_script_with_path = self.root_folder + '/evaluation/' + \
+          evaluation_script
+
+      jp = Job()
+      jp.setPythonExecutable(evaluation_script_with_path)
+      jp.addParam("data_dir", self.job_dir)
+      jp.addParam("localization_map", self.job["localization_map"])
+      if "parameter_file" in self.job:
+        jp.addParam("parameter_file", self.job["parameter_file"])
+      if "dataset" in self.job:
+        jp.addParam("dataset", self.job["dataset"])
+      jp.execute()
+
 
 if __name__ == '__main__':
-    
-    logging.basicConfig(level=logging.DEBUG)
-    logger = logging.getLogger(__name__)
 
-    parser = argparse.ArgumentParser(description="""Evaluate single job""")
-    parser.add_argument('job_dir', help='directory of the job', default='')
-    args = parser.parse_args()
-       
-    if args.job_dir:
-        j = Evaluation(args.job_dir)
-        j.runEvaluations()
+  logging.basicConfig(level=logging.DEBUG)
+  logger = logging.getLogger(__name__)
+
+  parser = argparse.ArgumentParser(description="""Evaluate single job""")
+  parser.add_argument('job_dir', help='directory of the job', default='')
+  args = parser.parse_args()
+
+  if args.job_dir:
+    j = Evaluation(args.job_dir)
+    j.runEvaluations()
--- python/utils.py
+++ python/utils.py
@@ -3,23 +3,26 @@
 import logging
 import sys
 
+
 def assertParam(options, parameter_name):
-    if parameter_name not in options:
-        raise ValueError(parameter_name + " not set")
-    
+  if parameter_name not in options:
+    raise ValueError(parameter_name + " not set")
+
+
 def checkParam(options, parameter_name, default_value):
-    if parameter_name not in options:
-        options[parameter_name] = default_value
-        logger = logging.getLogger(__name__)
-        logger.info("Setting default value for parameter '" + parameter_name  \
-                    + "' = "+str(default_value))
-                    
+  if parameter_name not in options:
+    options[parameter_name] = default_value
+    logger = logging.getLogger(__name__)
+    logger.info("Setting default value for parameter '" + parameter_name  \
+                + "' = " + str(default_value))
+
+
 def userYesNoQuery(question):
-    while True:
-      val = raw_input(question + " - Yes o No?\n").lower()
-      if val == 'y' or val == 'yes':
-        return True
-      elif val == 'n' or val == 'no':
-        return False
-      else:
-        sys.stdout.write('Please respond with \'y\' or \'n\'.\n')
+  while True:
+    val = raw_input(question + " - Yes o No?\n").lower()
+    if val == 'y' or val == 'yes':
+      return True
+    elif val == 'n' or val == 'no':
+      return False
+    else:
+      sys.stdout.write('Please respond with \'y\' or \'n\'.\n')
