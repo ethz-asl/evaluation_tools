3efe050afd938bab44c34f87b5d3315418a3567d
Kevin Egger <eggerk@ethz.ch>
2018-03-01 13:57:55 +0100

    Added test that correct number of jobs are creataed

--- test/test-job.py
+++ test/test-job.py
@@ -5,13 +5,14 @@ from __future__ import print_function
 from evaluation_tools.catkin_utils import catkinFindSrc
 from evaluation_tools.job import Job
 from evaluation_tools.run_experiment import Experiment
+import nose.tools
 import os
 
 RESULTS_FOLDER = './results'
 AUTOMATIC_DATASET_DOWNLOAD = True
 
 
-def test_create_job():
+def _create_jobs():
   experiment_path = os.path.join(
       catkinFindSrc('evaluation_tools'), 'experiments',
       'sample_experiment.yaml')
@@ -19,11 +20,25 @@ def test_create_job():
       experiment_path), 'File ' + experiment_path + ' not found.'
   experiment = Experiment(experiment_path, RESULTS_FOLDER,
                           AUTOMATIC_DATASET_DOWNLOAD)
-  jobs = experiment.job_list
-  print('Number of jobs:', len(jobs))
+  return experiment.job_list
+
+
+def test_job_creation():
+  NUMBER_OF_DATASETS = 1
+  NUMBER_OF_NORMAL_PARAMETER_FILES = 2
+  NUMBER_OF_PARAMETERS_IN_PARAMETER_SWEEP_FILES = [3]
+  NUMBER_OF_EXPECTED_JOBS = NUMBER_OF_DATASETS * (
+      NUMBER_OF_NORMAL_PARAMETER_FILES +
+      sum(NUMBER_OF_PARAMETERS_IN_PARAMETER_SWEEP_FILES))
+
+  jobs = _create_jobs()
+  nose.tools.eq_(len(jobs), NUMBER_OF_EXPECTED_JOBS)
+
+
+def test_load_job_from_folder():
+  jobs = _create_jobs()
   for job in jobs:
     print('Checking job:', job.job_path)
     job_from_file = Job()
     job_from_file.loadConfigFromFolder(job.job_path)
-    assert job == job_from_file, \
-        "Job %s wasn't loaded correctly from file" % job.job_path
+    nose.tools.eq_(job, job_from_file)
